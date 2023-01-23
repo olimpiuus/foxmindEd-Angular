b4ea97f326d8dd34a2430932b1864197bedf9e77
foxstudent103311 <kolegin.yuriu@gmail.com>
2023-01-22 21:31:35 -0700

    add generating template for main page

--- src/app/shop.ts
+++ src/app/shop.ts
@@ -23,13 +23,49 @@ export const products: IProduct[] = [
     attributes: ['free delivery', 'new']
   },
   {
-    id: 1,
+    id: 2,
     name: 'Orange',
     image:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Oranges_-_whole-halved-segment.jpg/1200px-Oranges_-_whole-halved-segment.jpg',
     price: 50,
     shop: 'Amazon',
     salePercent: 75,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, maiores ad quae voluptates provident deserunt blanditiis impedit corrupti amet quasi ratione earum odit laborum accusantium praesentium ducimus cumque, qui enim!',
+    attributes: ['free delivery', 'new']
+  },
+  {
+    id: 3,
+    name: 'Orange',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Oranges_-_whole-halved-segment.jpg/1200px-Oranges_-_whole-halved-segment.jpg',
+    price: 50,
+    shop: 'Amazon',
+    salePercent: 60,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, maiores ad quae voluptates provident deserunt blanditiis impedit corrupti amet quasi ratione earum odit laborum accusantium praesentium ducimus cumque, qui enim!',
+    attributes: ['free delivery', 'new']
+  },
+  {
+    id: 4,
+    name: 'Orange',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Oranges_-_whole-halved-segment.jpg/1200px-Oranges_-_whole-halved-segment.jpg',
+    price: 50,
+    shop: 'Amazon',
+    salePercent: 50,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, maiores ad quae voluptates provident deserunt blanditiis impedit corrupti amet quasi ratione earum odit laborum accusantium praesentium ducimus cumque, qui enim!',
+    attributes: ['free delivery', 'new']
+  },
+  {
+    id: 5,
+    name: 'Orange',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Oranges_-_whole-halved-segment.jpg/1200px-Oranges_-_whole-halved-segment.jpg',
+    price: 50,
+    shop: 'Amazon',
+    salePercent: 33,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, maiores ad quae voluptates provident deserunt blanditiis impedit corrupti amet quasi ratione earum odit laborum accusantium praesentium ducimus cumque, qui enim!',
     attributes: ['free delivery', 'new']
