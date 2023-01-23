f98fa5f7388490b44b44e174c696bcdb6228a5a8
foxstudent103311 <kolegin.yuriu@gmail.com>
2023-01-22 21:34:33 -0700

    change router and yellow coloor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { ContactComponent } from './contact/contact.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
-  { path: 'main', component: MainPageComponent },
+  { path: '', component: MainPageComponent },
   { path: 'contact', component: ContactComponent }
 ];
 
