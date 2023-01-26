125d1ea716b8149069e9d1dab55c18b8b257c4ae
foxstudent103311 <kolegin.yuriu@gmail.com>
2023-01-25 19:19:15 -0700

    add dist branch

--- src/app/limit-characters.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LimitCharactersPipe } from './limit-characters.pipe';
-
-describe('LimitCharactersPipe', () => {
-  it('create an instance', () => {
-    const pipe = new LimitCharactersPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
