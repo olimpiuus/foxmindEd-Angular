fbe049f5a36caf76aaff1f87fcb1ba317b99f10a
foxstudent103311 <kolegin.yuriu@gmail.com>
2023-03-01 22:52:47 -0700

    remove dump component from main module

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ShoppingItemComponent } from './shopping-item/shopping-item.component';
 import { ChangeItemBlockComponent } from './change-item-block/change-item-block.component';
-import { EditItemFormComponent } from './edit-item-form/edit-item-form.component';
 import { ShoppingItemDetailedComponent } from './shopping-item-detailed/shopping-item-detailed.component';
 import { BtnAddNewItemComponent } from './btn-add-new-item/btn-add-new-item.component';
 import { BikeDetailedComponent } from 'src/app/modules/main/bike-detailed/bike-detailed.component';
@@ -31,8 +30,7 @@ import { DataFetchService } from './data-fetch.service';
     LimitCharactersPipe,
     BikeDetailedComponent,
     BtnAddNewItemComponent,
-    ChangeItemBlockComponent,
-    EditItemFormComponent
+    ChangeItemBlockComponent
   ],
   imports: [
     CommonModule,
